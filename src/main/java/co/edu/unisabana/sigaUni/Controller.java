6528cf7d400348b409d3ed36d22ad60de9776adb
juan david sanchez <Juansangon@unisabana.edu.co>
2023-05-14 12:06:12 -0500

    AInicio de trabajo frontend + backend

--- src/main/java/co/edu/unisabana/sigaUni/Controller.java
+++ src/main/java/co/edu/unisabana/sigaUni/Controller.java
@@ -12,29 +12,23 @@ public class Controller {
 
     public Controller() {
         this.estudianteList = new ArrayList<>();
-        estudianteList.add(new Estudiante(2525, "Daniel", 3, "Ingenieria"));
-        estudianteList.add(new Estudiante(3030, "Samuel", 5, "Medicina"));
-        estudianteList.add(new Estudiante(1234, "Andrea", 4, "Ingenieria"));
-        estudianteList.add(new Estudiante(4321, "Jaime", 3, "Arquitectura"));
-        estudianteList.add(new Estudiante(9876, "Fulanita", 7, "Medicina"));
+        estudianteList.add(new Estudiante(2525, "Daniel", 3, "Ingenieria","Ingenieria Informática"));
+        estudianteList.add(new Estudiante(3030, "Samuel", 5, "Medicina", "Enfermería"));
+        estudianteList.add(new Estudiante(1234, "Andrea", 4, "Ingenieria", "Ingenieria Informática"));
+        estudianteList.add(new Estudiante(4321, "Jaime", 3, "Comunicación","Comunicación Social"));
+        estudianteList.add(new Estudiante(9876, "Fulanita", 7, "Derecho", "Filosofía"));
     }
 
     @GetMapping(path = "/estudiantes")
-    public List<Estudiante> obtenerEstudiantesPorFacultad(@RequestParam String facultad, @RequestParam(required = false) Integer cantidad) {
+    public List<Estudiante> obtenerEstudiantesPorFacultad(@RequestParam String facultad) {
         List<Estudiante> busqueda = new ArrayList<>();
-        int contador = 0;
         for (Estudiante estudiante : estudianteList) {
             if (estudiante.getFacultad().equals(facultad)) {
                 busqueda.add(estudiante);
-                contador++;
-                if (cantidad != null && contador >= cantidad) {
-                    break;
-                }
             }
         }
         return busqueda;
     }
-
 
     @GetMapping(path = "/estudiantes/todos")
     public List<Estudiante> obtenerEstudiantes() {
@@ -43,33 +37,33 @@ public class Controller {
 
 
     @PostMapping(path = "/estudiante/crear")
-    public String crearEstudiante(@RequestBody Estudiante estudiante) {
+    public Respuesta crearEstudiante(@RequestBody Estudiante estudiante) {
         estudiante.setId((int) (Math.random() * 1000));
         estudianteList.add(estudiante);
-        return "Estudiante ingresado correctamente";
+        return new Respuesta("Estudiante ingresado correctamente");
     }
 
     @DeleteMapping(path = "/estudiante/eliminar/{id}")
-    public String eliminarEstudiante(@PathVariable int id) {
+    public Respuesta eliminarEstudiante(@PathVariable int id) {
         boolean eliminado = estudianteList.removeIf(estudiante -> estudiante.getId() == id);
         if (eliminado) {
-            return "El estudiante se ha eliminado";
+            return new Respuesta("Estudiante eliminado correctamente");
         } else {
-            return "No se encontró ningún estudiante con el ID proporcionado, intente nuevamente";
-        }
+            return new Respuesta("No se encontró ningún estudiante con el ID proporcionado, intente nuevamente");        }
     }
 
     @PutMapping(path = "estudiante/editar/{id}")
-    public String editarEstudiante(@PathVariable int id, @RequestBody Estudiante estudianteActualizado) {
+    public Respuesta editarEstudiante(@PathVariable int id, @RequestBody Estudiante estudianteActualizado) {
         for (Estudiante estudiante : estudianteList) {
             if (estudiante.getId() == id) {
                 estudiante.setNombre(estudianteActualizado.getNombre());
                 estudiante.setSemestre(estudianteActualizado.getSemestre());
                 estudiante.setFacultad(estudianteActualizado.getFacultad());
-                return "El estudiante se ha editado :) ";
+                estudiante.setPrograma(estudianteActualizado.getPrograma());
+                return new Respuesta("El estudiante se ha editado :) ");
             }
         }
-        return "No se encontró ningún estudiante con el ID proporcionado, intente nuevamente ";
+        return new Respuesta("No se encontró ningún estudiante con el ID proporcionado, intente nuevamente ");
     }
 
 
